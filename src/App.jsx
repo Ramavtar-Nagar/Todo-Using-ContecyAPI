1f91b05e1e97024c1ceefaf8c86542206bae08e4
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-07-01 13:54:52 +0530

    added localstorage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,17 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.toggleComplete} : prevTodo))
   }
 
-  
+  useEffect(() => {
+    
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if( todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
