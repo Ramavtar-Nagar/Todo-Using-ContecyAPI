b15610c2ac03bb65ad8f67554527c388c0a8b7fe
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-07-02 13:13:55 +0530

    Project completed

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -7,6 +7,15 @@ function TodoItem({ todo }) {
     const [todoMsg, setTodoMsg] = useState(todo.todo)
     const {updateTodo, toggleComplete, deleteTodo} = useTodo()
 
+    const editTodo = () => {
+        updateTodo(todo.id, {...todo, todo: todoMsg})
+        setIsTodoEditable(false)
+    }
+
+    const toggleCompleted = () => {
+        toggleComplete(todo.id)
+    }
+
     return (
         <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
